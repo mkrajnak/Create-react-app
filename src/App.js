d6c8649b95848f099dd888bccaa5cdfe9bff400b
mkrajnak <krajnakmatto@gmail.com>
2016-10-05 01:54:24 +0200

    One does not simply write an react app

--- src/App.js
+++ src/App.js
@@ -1,104 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
 
-// Stateless component
-const Button = (props) => <button onClick={props.onClick}>{props.name}</button>;
-
+const Button = (props) => <button onClick={props.onClick}>{props.name}</button>
 // Statefull component
-// Using react logo for component lifecycle methods demostration
-class ReactLogo extends Component{
-  componentWillMount(){
-    console.log("Mouting logo")
+const Counter = ({value, onIncrement, onDecrement}) => {
+  return (
+    <div>
+      <h1>Counter </h1>
+      <Button name='+' onClick={onIncrement()}></Button>
+      <Button name='-' onClick={onDecrement()}></Button>
+    </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+  return {
+
   }
-  render(){
-    console.log("Rendering logo")
-    if (this.props.logoIsViewed) {
-      return <img src={logo} className="App-logo" alt="logo" />
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIncrement: () => {
+      dispatch({type: 'INCREMENT'})
+    },
+    onDecrement: () => {
+      dispatch({type: 'DECREMENT'})
     }
-    else {
-      return <h2>Click on button to toggle logo</h2>
-    }
-  }
-  componentWillDidMount(){
-    console.log("Logo mounted")
-  }
-  // does nothing
-  componentWillUnmount(){
-    console.log("Delete component")
-  }
-  componentWillReceiveProps(nextProps){
+  };
+};
 
-  }
-  shouldComponentUpdate(nextProps, nextState){
-    return true
-  }
-  componentDidUpdate(prevProps, prevState){
-    
-  }
-}
-
-// Statefull component
-class Counter extends Component{
-  constructor(props){
-    super(props);
-    this.state = {counts: props.initialCount}
-    this.increment = this.increment.bind(this)
-    this.decrement = this.decrement.bind(this)
-  }
-  increment(){
-    this.setState({counts: (this.state.counts + 1)});
-  }
-  decrement(){
-    this.setState({counts: (this.state.counts - 1)});
-  }
-  render(){
-    return (
-      <div>
-        <h1>{this.props.name} {this.state.counts}</h1>
-        <Button name="Increment" onClick={this.increment}/>
-        <Button name="Decrement" onClick={this.decrement}/>
-      </div>
-    );
-  }
-}
-
-// define type of props
-Counter.propTypes = {
-  name: React.PropTypes.string.isRequired,
-  initialCount: React.PropTypes.number
-}
-// default values
-Counter.defaultProps = {
-  name: 'Counting:',
-  initialCount: 0
-}
+const VisibleCounter = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter)
 
 // main componenet
-class App extends Component {
-  constructor(){
-    super()
-    this.state = {logoIsViewed: true}
-    this.updateLogo = this.updateLogo.bind(this)
-  }
-  updateLogo(){
-    this.setState({logoIsViewed: !this.state.logoIsViewed})
-  }
-  render() {
-    return (
+const App = () => (
       <div className="App">
         <div className="App-header">
           <h2>Welcome to React</h2>
-          <ReactLogo logoIsViewed={this.state.logoIsViewed}/>
+          <img src={logo} className="App-logo" alt="logo" />
         </div>
         <br/>
-        <Button name="Toggle logo" onClick={this.updateLogo}/>
-        <hr/>
-        <Counter/>
-        <hr/>
+        <VisibleCounter/>
       </div>
-    );
-  }
-}
+  )
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
+const store = createStore(App)
+
+const render = () => ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </ Provider>,
   document.getElementById('root')
 );
+
+render();
