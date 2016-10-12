07fa052be93057fc2962bf0dd5ba6805d03cd78f
mkrajnak <krajnakmatto@gmail.com>
2016-10-13 00:48:02 +0200

    correcting reducer

--- src/counting.js
+++ src/counting.js
@@ -1,11 +1,11 @@
 const counter = (state = {value: 0}, action) => {
   switch (action.type) {
       case 'INCREMENT':
-        return Object.assign({
+        return Object.assign({}, state, {
           value: state.value + 1
         })
       case 'DECREMENT':
-        return Object.assign({
+        return Object.assign({}, state,{
         value: state.value - 1
       })
       default:
